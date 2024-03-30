e63831595c0b8fac11781b255bc52cd40bb85f6c
raulathallah <raultaqi@gmail.com>
2024-03-30 14:46:08 +0700

    my future app frontend first commit

---
+++ src/app/axios/action.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { baseURL } from "../_lib/baseURL";
+
+export const getAllAction = async () => {
+  const response = await axios.get(baseURL + "/api/action/get-all");
+  return response;
+};
+
+export const createAction = async (body: ActionBody) => {
+  const response = await axios.post(baseURL + "/api/action/create", body);
+  return response;
+};
+
+export const deleteAction = async (id: string) => {
+  const response = await axios.delete(baseURL + `/api/action/delete/${id}`);
+  return response;
+};
---
+++ src/type.ts
@@ -0,0 +1,13 @@
+type Action = {
+  _id: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  //"__v": 0
+};
+
+type ActionBody = {
+  title: string;
+  description: string;
+};
