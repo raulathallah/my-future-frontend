fd6dcee168c5e214329947507f1e3d5d738dceaa
raulathallah <raultaqi@gmail.com>
2024-04-01 15:08:35 +0700

    deployment

--- src/app/_lib/baseURL.ts
+++ src/app/_lib/baseURL.ts
@@ -1,2 +1,2 @@
-//export const baseURL = "https://my-future-backend.vercel.app";
-export const baseURL = "http://localhost:5000";
+export const baseURL = "https://my-future-backend.vercel.app";
+//export const baseURL = "http://localhost:5000";
